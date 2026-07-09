d5c17c96500f510a95f6a38d2e1e373545dd77a9
dyhia REHOUNE <dihia.rahoun94@yahoo.fr>
2026-07-09 15:33:27 +0000

    feat: add staging layer for all source tables

--- models/staging/stg_localbike_database__order_items.sql
+++ models/staging/stg_localbike_database__order_items.sql
@@ -3,6 +3,6 @@ SELECT
   item_id, 
   product_id, 
   quantity, 
-  list_price, 
-  discount
+  SAFE_CAST(list_price AS NUMERIC) AS list_price, 
+  SAFE_CAST(discount AS NUMERIC) AS discount
 FROM {{ source('localbike_database', 'order_items') }}
